0146a6faa3ec143c70f1739277ec02a870c29a39
rocketjishao <44188353+rocketjishao@users.noreply.github.com>
2020-02-16 18:15:02 +0800

    Sort the tagged and untagged RNA

--- main.py
+++ main.py
@@ -4,11 +4,13 @@
 import sys
 def fun(argv):
     input_file = argv[0]
-    output_file = argv[1]
+    tagged_file = argv[1]
+    untagged_file = argv[2]
     print 'input file:   %s' % (input_file)
-    print 'search range: 40'
+    print 'search range: fist 40nt'
     print 'subsequence:  GAACCUGAACCU(12nt)' 
-    print 'output file:  %s' % (output_file)
+    print 'tagged file:  %s' % (tagged_file)
+    print 'untagged file:%s' % (untagged_file)
     records = []
     with open(input_file, 'r') as f:
         record = []
@@ -17,24 +19,31 @@ def fun(argv):
             if len(record) == 4:
                 records.append(record)
                 record = []
-    match_count = 0
-    with open(output_file, 'w') as f:
+    tagged_count = 0
+    untagged_count = 0
+    with open(tagged_file, 'w') as f, open (untagged_file, 'w') as g:
         for record in records:
             sequence = record[1][:40]
             if sequence.find('GAACCUGAACCU') < 0 and sequence.find('AACCUGAACCUG') < 0 and sequence.find('ACCUGAACCUGA') < 0 and sequence.find('CCUGAACCUGAA') < 0 and sequence.find('CUGAACCUGAAC') < 0 and sequence.find('UGAACCUGAACC') < 0:
-                continue
-            # print sequence
-            match_count = match_count + 1
+            	untagged_count = untagged_count + 1
+		# print untagged sequence
+	        for r in record:
+		    if r == '':
+		        continue
+		    g.write(r + '\n')
+		continue
+            # print tagged sequence
+            tagged_count = tagged_count + 1
             for r in record:
                 if r == '':
                     continue
                 f.write(r + '\n')
-    print 'total [%d] match [%d]' % (len(records), match_count)
+    print 'total [%d] tagged [%d] untagged [%d]' % (len(records), tagged_count, untagged_count)
 if __name__ == '__main__':
-    run_format  = '  python main.py input.file output.file'
-    run_example = '  python main.py input.fastq output.fastq'
+    run_format  = '  python main.py input.file tagged.file untagged.file'
+    run_example = '  python main.py input.fastq tagged.file untagged.file'
     print len(sys.argv)
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print 'you need follow this run format'
         print run_format
         print run_example
